80d4bc19a9e704abfb96b4be2d9710f395159e2c
TranDanh1122 <trandanh14042000@gmail.com>
2025-04-04 20:00:18 +0700

    update debound func

--- app/src/hooks/useDebound.ts
+++ app/src/hooks/useDebound.ts
@@ -1,5 +1,10 @@
 import React from "react"
-
+/**
+ * 
+ * @param callback : hàm cần debounce, hàm này sẽ được gọi sau 1 khoảng thời gian (delay)
+ * @param delay : thời gian delay (ms)
+ * @returns hàm nhận vào các tham số của callback, sẽ thực thi callbacl trong quá trình thực thi (sau debound time)
+ */
 export const useDebound = (callback: (...args: any) => void, delay: number) => {
     const deboundRef = React.useRef<number | null>(null)
     const handler = (...args: any) => {
