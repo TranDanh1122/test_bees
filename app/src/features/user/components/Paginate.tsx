5de5d8501538b8f3b96552868cde32da3487fd6e
TranDanh1122 <trandanh14042000@gmail.com>
2025-04-04 20:35:58 +0700

    paginate action

--- app/src/features/user/components/Paginate.tsx
+++ app/src/features/user/components/Paginate.tsx
@@ -8,31 +8,68 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination"
+import { UserListContext } from "@/features/user";
 
 /**
  * React component render phần chọn trang / phân trang
  *  
  */
 export default React.memo(function Paginate(): React.JSX.Element {
+    const { state, dispatch } = React.useContext(UserListContext)
+    const goToPage = (page: number) => {
+        if (page >= 1 && page <= state.numberOfPage && page !== state.page) {
+            dispatch({ type: "goToPage", payload: page });
+        }
+    };
     return (
         <Pagination className="w-fit mx-0 [&__a]:cursor-pointer [&__a]:border">
             <PaginationContent>
                 <PaginationItem>
-                    <PaginationPrevious />
+                    <PaginationPrevious onClick={() => goToPage(state.page - 1)} />
                 </PaginationItem>
+                {
+                    state.page > 3 &&
+                    <PaginationItem>
+                        <PaginationLink onClick={() => goToPage(1)} > {1}</PaginationLink>
+                    </PaginationItem>
+                }
+                {state.page > 2 &&
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                }
+                {
+                    state.page > 1 &&
+                    <PaginationItem>
+                        <PaginationLink onClick={() => goToPage(state.page - 1)} > {state.page - 1}</PaginationLink>
+                    </PaginationItem>
+                }
                 <PaginationItem>
-                    <PaginationLink >1</PaginationLink>
+                    <PaginationLink className="text-[var(--hover-color)] border-[var(--hover-color)]">{state.page}</PaginationLink>
                 </PaginationItem>
+                {
+                    state.page < state.numberOfPage &&
+                    <PaginationItem>
+                        <PaginationLink onClick={() => goToPage(state.page + 1)}>{state.page + 1}</PaginationLink>
+                    </PaginationItem>
+                }
+                {
+                    state.page < state.numberOfPage - 2 &&
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                }
+                {
+                    state.page < state.numberOfPage - 1 &&
+                    <PaginationItem>
+                        <PaginationLink onClick={() => goToPage(state.numberOfPage)}>{state.numberOfPage}</PaginationLink>
+                    </PaginationItem>
+                }
+
                 <PaginationItem>
-                    <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationLink >4</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationNext />
+                    <PaginationNext onClick={() => goToPage(state.page + 1)} />
                 </PaginationItem>
             </PaginationContent>
-        </Pagination>
+        </Pagination >
     )
 })
