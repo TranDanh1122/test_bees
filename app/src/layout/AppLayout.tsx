393410ad595006ea3416a0ae620fd707348974c2
TranDanh1122 <trandanh14042000@gmail.com>
2025-04-04 18:52:37 +0700

    fix header bg

--- app/src/layout/AppLayout.tsx
+++ app/src/layout/AppLayout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout({ children }: Props): React.JSX.Element {
     return (
         <QueryClientProvider client={queryClient} >
             <div className="min-w-screen min-h-screen relative">
-                <picture className="w-screen h-80 absolute top-0 left-0">
+                <picture className="w-full h-80 absolute top-0 left-0">
                     <source media="(max-width:767px)" srcSet="assets/bg-light-mobile.jpg" />
                     <source media="(min-width:768px)" srcSet="assets/bg-light.jpg" />
                     <img src="assets/bg-light.jpg" className="size-full object-cover" />
