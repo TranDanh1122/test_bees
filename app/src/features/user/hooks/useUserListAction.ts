35a29b3878703f18b36fb3f7c3ffbf7d4e8689b0
TranDanh1122 <trandanh14042000@gmail.com>
2025-04-05 22:25:03 +0700

    context handle eror

--- app/src/features/user/hooks/useUserListAction.ts
+++ app/src/features/user/hooks/useUserListAction.ts
@@ -4,7 +4,11 @@ import { UserListContext } from "@/features/user";
  * Custom hook quản lí các hành động với bảng user
  */
 export const useUserListAction = () => {
-    const { state, dispatch } = React.useContext(UserListContext)
+    const context = React.useContext(UserListContext)
+    if (!context) {
+        throw new Error("Out of Provider");
+    }
+    const { state, dispatch } = context
     const setLimit = React.useCallback((value: string) => dispatch({ type: "setLimit", payload: Number(value) }), [])
     const goToPage = React.useCallback((page: number) => {
         if (page >= 1 && page <= state.numberOfPage && page !== state.page) {
@@ -13,5 +17,5 @@ export const useUserListAction = () => {
     }, [state.numberOfPage, state.page, dispatch])
     const handleSearch = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => dispatch({ type: "search", payload: e.target.value }), [])
     const handleFilter = React.useCallback((value: unknown) => dispatch({ type: "setFilterStatus", payload: value as unknown as boolean }), [])
-    return { setLimit, goToPage, handleSearch, handleFilter, state , dispatch}
+    return { setLimit, goToPage, handleSearch, handleFilter, state, dispatch }
 }
